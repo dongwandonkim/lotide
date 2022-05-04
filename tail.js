3a40ac637ef759a50774cb2be2e600f8c4d05a19
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-03 20:27:04 -0700

    eqArray

--- tail.js
+++ tail.js
@@ -14,8 +14,9 @@ const tail = (arr) => {
   console.log(tails, arr);
 };
 
-// const result = tail(['Hello', 'Lighthouse', 'Labs']);
-// assertEqual(result, ['Lighthouse', 'Labs']);
+const result = tail(['Hello', 'Lighthouse', 'Labs']);
+assertEqual(result, ['Lighthouse', 'Labs']);
+
 const words = ['Yo Yo', 'Lighthouse', 'Labs'];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3);
