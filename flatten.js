0ced63ee8095a6044d5b16439f9ef7f65e0f7fab
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-03 22:47:19 -0700

    flatten

---
+++ flatten.js
@@ -0,0 +1,16 @@
+const flatten = (arr) => {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let y = 0; y < arr[i].length; y++) {
+        newArr.push(arr[i][y]);
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+
+console.log(flatten([1, [2, 3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
