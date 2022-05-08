a0f2c51c67dbca4ecfc75757a7e6d9fb8a80a80a
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-07 22:20:20 -0700

    obj recursion

--- eqObjects.js
+++ eqObjects.js
@@ -25,9 +25,14 @@ const eqObjects = function (object1, object2) {
   for (let key of objOneKeys) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
-        console.log('arrayyyy');
         return false;
       }
+    } else if (
+      typeof object1[key] === 'object' &&
+      typeof object2[key] === 'object'
+    ) {
+      console.log('hi');
+      return eqObjects(object1[key], object2[key]);
     } else if (object1[key] !== object2[key]) {
       console.log('obj1', object1[key], 'obj2', object2[key]);
       return false;
@@ -43,9 +48,15 @@ const eqObjects = function (object1, object2) {
 // const abc = {a: '1', b: '2', c: '3'};
 // assertEqual(eqObjects(ab, abc), false); // => false
 
-const cd = {c: '1', d: ['2', 3], a: {g: 123}};
-const dc = {d: ['2', 3], c: '1', a: {g: 123}};
-assertEqual(eqObjects(cd, dc), true); // => true
+const cd = {c: '1', d: ['2', 3], a: {g: {h: 123}}};
+const dc = {d: ['2', 3], c: '1', a: {g: {h: 123}}};
+
+const cd3 = {c: '1', d: ['2', 3], a: {g: {h: 123, i: [5, 4, 3, 2, 1]}}};
+const dc4 = {d: ['2', 3], c: '1', a: {g: {h: 123, i: [5, 4, 3, 2, 1]}}};
+
+const cd5 = {c: '1', d: ['2', 3], a: {g: {h: 123, i: [5, 4, 3, 2, '1']}}};
+const dc6 = {d: ['2', 3], c: '1', a: {g: {h: 123, i: [5, 4, 3, 2, 1]}}};
+assertEqual(eqObjects(cd3, dc4), true); // => true
 
 const cd2 = {c: '1', d: ['2', 3, 4]};
 // assertEqual(eqObjects(cd, cd2), false); // => false
