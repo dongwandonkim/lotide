6d9aa0269a28c32fc68eda02d2470bf813e27b02
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-07 22:32:16 -0700

    eqArray recursion

--- eqArrays.js
+++ eqArrays.js
@@ -11,15 +11,21 @@ const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) return false;
 
   for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      // console.log(arr1[i], arr2[i]);
+      return eqArrays(arr1[i], arr2[i]);
+    }
     if (arr1[i] !== arr2[i]) return false;
   }
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+const nested = [1, [2, 5, [6, 7]], 3];
+const nested2 = [1, [2, 5, [6, 7]], 3];
+console.log(eqArrays(nested, nested2)); // => true
+// // console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(['1', '2', '3'], ['1', '2', '3'])); // => true
-console.log(eqArrays(['1', '2', '3'], ['1', '2', 3])); // => false
+// // console.log(eqArrays(['1', '2', '3'], ['1', '2', '3'])); // => true
+// // console.log(eqArrays(['1', '2', '3'], ['1', '2', 3])); // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
