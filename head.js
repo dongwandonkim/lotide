0f9880f2c456f1c4212e38574f6bd92754cf1e9b
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-09 14:03:57 -0700

    refactored assertEqual

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = (arr) => {
   // if (!arr.length) return undefined;
