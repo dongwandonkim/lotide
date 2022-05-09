a2d5a2c54ab6e8580922de440ddd940aa56f7cf7
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-09 14:15:29 -0700

    eqArray & assertArraysEqual refactor

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) return false;
 
@@ -20,12 +11,4 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
-const nested = [1, [2, 5, [6, 7]], 3];
-const nested2 = [1, [2, 5, [6, 7]], 3];
-console.log(eqArrays(nested, nested2)); // => true
-// // console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-// // console.log(eqArrays(['1', '2', '3'], ['1', '2', '3'])); // => true
-// // console.log(eqArrays(['1', '2', '3'], ['1', '2', 3])); // => false
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
