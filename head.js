9b8cae92ec992100f8eadad181052c72572253c2
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-09 14:08:27 -0700

    refactored tail

--- head.js
+++ head.js
@@ -4,7 +4,5 @@ const head = (arr) => {
   // if (!arr.length) return undefined;
   return arr[0];
 };
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual(head([]), 5);
+
+module.exports = head;
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,21 @@
 const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+const head = require('../head');
 
 // TEST CODE
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
+// assertEqual('Lighthouse Labs', 'Bootcamp');
+// assertEqual(1, 1);
+
+// HEAD
+// TEST CODE
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+assertEqual(head([]), 5);
+
+// TAIL
+const result = tail(['Hello', 'Lighthouse', 'Labs']);
+assertEqual(result, ['Lighthouse', 'Labs']);
+
+const words = ['Yo Yo', 'Lighthouse', 'Labs'];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
