4e9a6d1b87a11c9ac550fc02ce32a63c74fd3a5c
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-09 17:22:30 -0700

    tail & middle Tests

--- tail.js
+++ tail.js
@@ -2,7 +2,8 @@ const tail = (arr) => {
   if (arr.length <= 1) return [];
 
   const tails = arr.slice(1);
-  console.log(tails, arr);
+  console.log(tails);
+  return tails;
 };
 
 module.exports = tail;
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,15 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
 
 // assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 // assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+const assert = require('chai').assert;
+
+describe('#middle', () => {
+  it('returns middle values from the passed in Array', () => {
+    const actual = [1, 2, 3, 4, 5];
+    const expected = [3];
+    assert.deepEqual(middle(actual), expected);
+  });
+});
